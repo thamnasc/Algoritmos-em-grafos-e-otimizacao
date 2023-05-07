4ffafbd85858bb6375a9944b86108687c3ece389
thamnasc <thalitanas09@gmail.com>
2023-05-06 23:30:15 -0300

    FIX leak problems

--- Exercicio-Grafos/grafo.c
+++ Exercicio-Grafos/grafo.c
@@ -50,18 +50,14 @@ grafo cria_grafo() {
 
 // destroi grafo G (desaloca toda a memoria)
 void destroi_grafo(grafo G) {  
-  vertice v;
-  aresta a;
+  vertice V;
+  aresta A;
   while (!vazio(G->vertices))
   {
-    v = (vertice) desempilha(G->vertices);
-    remove_vertice(vertice_id(v), G);
+    V = (vertice) topo(G->vertices);
+    remove_vertice(vertice_id(V), G);
   }
-/*   while (!vazio(G->arestas))
-  {
-    a = desempilha(G->arestas);
-    remove_aresta(a, G);
-  } */
+
   free(G->arestas);
   free(G->vertices);
   free(G);
@@ -80,39 +76,20 @@ void adiciona_vertice(int id, grafo G) {
 // remove vertice com id <id> do grafo G e o destroi
 // [deve remover e destruir tambem as arestas incidentes]
 void remove_vertice(int id, grafo G) {
-  //primeiro remove a aresta, depois o vertice, para não perder o obj
   vertice V = (vertice) (busca_chave(id, vertices(G), (int_f_obj) vertice_id));
-  aresta cont_AdeV, cont_AdeG;
-  printf("teste1\n\n");
-  no aresta_G = primeiro_no(arestas(G));
-  printf("teste2\n\n");
-  no aresta_V = primeiro_no(fronteira(V));
-  printf("teste3\n\n");
-  // percorre a lista de fronteira do vértice a ser removido
-  while (aresta_V != NULL)
+  aresta A;
+  vertice u_A, v_A;
+
+  while(!vazio(fronteira(V)))
   {
-    // percorre a lista de arestas do grafo
-    while (aresta_G != NULL)
-    {
-      cont_AdeV = (aresta) (conteudo(aresta_V));
-      cont_AdeG = (aresta) (conteudo(aresta_G));
-      // se a aresta da vez for {U, V}
-      if (cont_AdeG == cont_AdeV)
-      {
-        // remove aresta {U, V} de cada vértice
-        remove_chave(aresta_id(cont_AdeV), fronteira(vertice_u(cont_AdeV)), (int_f_obj) aresta_id);
-        printf("teste4\n\n");
-        remove_chave(aresta_id(cont_AdeV), fronteira(vertice_v(cont_AdeV)), (int_f_obj) aresta_id);
-        printf("teste5\n\n");
-        // remove aresta {U, V} do grafo
-        remove_aresta(aresta_id(cont_AdeG), G);
-      }
-      aresta_G = proximo(aresta_G);
-    }
-    aresta_V = proximo(aresta_V);
+    A = (aresta) topo(fronteira(V));
+    u_A = vertice_u(A);
+    v_A = vertice_v(A);
+    remove_chave(aresta_id(A), fronteira(u_A), (int_f_obj) aresta_id);
+    remove_chave(aresta_id(A), fronteira(v_A), (int_f_obj) aresta_id);
+    remove_aresta(aresta_id(A), G);
   }
-  // remove V da lista de vertices do grafo
-  remove_chave(id, vertices(G), (int_f_obj) vertice_id);
+  remove_chave(vertice_id(V), vertices(G), (int_f_obj) vertice_id);
   free(V->fronteira);
   free(V);
 }
